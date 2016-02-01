95eeeff528cceb2bf6e324668eeb18fed1b752ac
devalfrz <alfredo.rius@gmail.com>
2016-02-01 00:43:54 -0600

    Update arduino-lockitron-nfc.ino

--- Arduino/arduino-lockitron-nfc/arduino-lockitron-nfc.ino
+++ Arduino/arduino-lockitron-nfc/arduino-lockitron-nfc.ino
@@ -18,7 +18,7 @@
           it will either save the current card and blink 3 times or delete the
           card and blink one time for 2 seconds.
           
-      - SLOT_SELECT: Keep this button pressed and this will clear all cards in
+      - RESET_MEMORY: Keep this button pressed and this will clear all cards in
           memory.
           
 
